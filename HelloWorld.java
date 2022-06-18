0135a8fe2ea4214a478c0a3ce81785e229a5fc04
narayana <m.narayana236@gmail.com>
2022-06-18 10:59:22 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class HelloWorld {
+	String name;
+	static String city;
+	int variation = 0;
+
+	public static void main(String args[]) {
+		
+		HelloWorld hello = new HelloWorld();
+		hello.name = "Sudha";
+		HelloWorld.city = "Markapuram";
+
+		HelloWorld hello1 = new HelloWorld();
+		hello1.name = "Ravi";
+		HelloWorld.city = "Kanigiri";
+
+		System.out.println("Name : " + hello.name + ", City : " + HelloWorld.city);
+		System.out.println("Name : " + hello1.name + ", City : " + HelloWorld.city);
+	}
+
+	public static int sub(int a, int b) {
+		return a-b;
+	}
+
+	public static int sum(int a, int b) {
+		return variation + a + b;
+	}
+
+}
